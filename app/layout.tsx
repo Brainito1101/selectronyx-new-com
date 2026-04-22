8c25e111a1a7a265dc39cb6d705f31796aca7ba1
Nidhi Mevada <accounts@brainito.com>
2026-04-22 14:50:25 +0530

    Updated leadinfo tracking ID from LI-69D5F9762482F to LI-69E89165B0F8D

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${montserrat.variable} font-sans antialiased`}>
+      <body className={`${montserrat.variable} font-sans antialiased`}>       
         <DeferredAnalytics />
 
 
@@ -171,7 +171,7 @@ export default function RootLayout({
             __html: `(function(l,e,a,d,i,n,f,o){if(!l[i]){l.GlobalLeadinfoNamespace=l.GlobalLeadinfoNamespace||[];
 l.GlobalLeadinfoNamespace.push(i);l[i]=function(){(l[i].q=l[i].q||[]).push(arguments)};l[i].t=l[i].t||n;
 l[i].q=l[i].q||[];o=e.createElement(a);f=e.getElementsByTagName(a)[0];o.async=1;o.src=d;f.parentNode.insertBefore(o,f);}
-}(window,document,'script','https://cdn.leadinfo.net/ping.js','leadinfo','LI-69D5F9762482F'));`,
+}(window,document,'script','https://cdn.leadinfo.net/ping.js','leadinfo','LI-69E89165B0F8D'));`,
           }}
         />
       </body>
